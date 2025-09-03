d5b6ed7324af59311bb5e025df2b3414624b93ea
gauravtrs <gauravpateltrs@gmail.com>
2025-09-03 18:16:06 +0530

    product model/table created

---
+++ backend/models/productModel.js
@@ -0,0 +1,105 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // User model ka sahi ref
+      required: true,
+    },
+
+    name: {
+      type: String,
+      required: true,
+    },
+
+    rating: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 5,
+    },
+
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+
+
+
+
+
+const productSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // User model ka sahi ref
+      required: true,
+    },
+
+    name: {
+      type: String,
+      required: true,
+    },
+
+    image: {
+      type: String,
+      required: true,
+    default: "https://res.cloudinary.com/gauravkacloud/image/upload/v1731986753/photo_yrra2i.png", 
+
+    },
+
+    brand: {
+      type: String,
+      required: true,
+    },
+
+    category: {
+      type: String,
+      required: true,
+      enum: ["Chair", "Bed", "Double Bed", "Table", "Lamp", "Almira", "Doors"],
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    reviews: [reviewSchema],
+
+    rating: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 5,
+      default: 0,
+    },
+
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema); 
+
+export default Product; 
